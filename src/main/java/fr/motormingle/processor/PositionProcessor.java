448419fdb786e372ca7ef407eb4ea96ddd62540c
Lebarbanchon Valentin <valentin.lebarbanchon@ecole.ensicaen.fr>
2023-06-09 20:15:50 +0100

    2-create-encounterjob Encounter Job Working, Fixed snakeyaml vulnerability

--- src/main/java/fr/motormingle/processor/PositionProcessor.java
+++ src/main/java/fr/motormingle/processor/PositionProcessor.java
@@ -1,33 +1,56 @@
 package fr.motormingle.processor;
 
+import com.uber.h3core.H3Core;
 import fr.motormingle.model.Encounter;
 import fr.motormingle.model.Position;
 import fr.motormingle.model.Status;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class PositionProcessor implements ItemProcessor<List<Position>, List<Encounter>> {
 
     @Override
-    public List<Encounter> process(@NotNull List<Position> items) throws Exception {
+    public List<Encounter> process(List<Position> items) throws Exception {
         List<Encounter> encounters = new ArrayList<>();
-        items.forEach((item) -> {
-            Encounter encounter = new Encounter();
-            encounter.setUserId1(item.getUserId());
-            encounter.setUserId2(item.getUserId());
-            encounter.setHash("8a283082a677fff");
-            encounter.setDate(items.get(0).getDate().toLocalDate().plusDays(10L));
-            encounter.setCount(1);
-            encounter.setStatus(Status.PENDING.toString());
+        Map<String, List<Position>> h3Positions = new HashMap<>();
+        H3Core h3 = H3Core.newInstance();
 
-            encounters.add(encounter);
+        items.forEach((item) -> {
+            String h3Index = h3.latLngToCellAddress(item.getLatitude(), item.getLongitude(), 8);
+            if (h3Positions.containsKey(h3Index)) {
+                h3Positions.get(h3Index).add(item);
+            } else {
+                List<Position> positions = new ArrayList<>();
+                positions.add(item);
+                h3Positions.put(h3Index, positions);
+            }
         });
-        System.err.println(encounters);
+
+        h3Positions.forEach((h3Index, positions) -> {
+            if (positions.size() < 2) {
+                return;
+            }
+            for (int i = 0; i < positions.size() - 1; i++) {
+                for (int j = i + 1; j < positions.size(); j++) {
+                    Position position1 = positions.get(i);
+                    Position position2 = positions.get(j);
+                    Encounter encounter = new Encounter();
+                    encounter.setUserId1(position1.getUserId());
+                    encounter.setUserId2(position2.getUserId());
+                    encounter.setHash(h3Index);
+                    encounter.setDate(position1.getDate().toLocalDate());
+                    encounter.setCount(1);
+                    encounter.setStatus(Status.PENDING.toString());
+                    encounters.add(encounter);
+                }
+            }
+        });
 
         return encounters.size() == 0 ? null : encounters;
     }
--- src/main/java/fr/motormingle/writer/EncounterWriter.java
+++ src/main/java/fr/motormingle/writer/EncounterWriter.java
@@ -19,23 +19,24 @@ public class EncounterWriter implements ItemWriter<List<Encounter>> {
     private DataSource datasource;
 
     @Override
-    public void write(@NotNull Chunk<? extends List<Encounter>> encounters) throws Exception {
+    public void write(@NotNull Chunk<? extends List<Encounter>> encounters) {
+        if (encounters.getItems().size() == 0) {
+            return;
+        }
         JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource);
-        encounters.getItems().get(0).forEach((encounter) -> {
-            jdbcTemplate.update("INSERT INTO encounter(hash, count, date, status, user_id_1, user_id_2) " +
-                            "VALUES (?, ?, ?, ?, ?, ?) " +
-                            "ON CONFLICT ON CONSTRAINT pk_encounter " +
-                            "DO UPDATE SET count = (SELECT count FROM encounter WHERE user_id_1 = ? AND user_id_2 = ?) + 1, date = ?, hash = ?",
-                    encounter.getHash(),
-                    encounter.getCount(),
-                    Timestamp.valueOf(encounter.getDate().atStartOfDay()),
-                    encounter.getStatus(),
-                    encounter.getUserId1(),
-                    encounter.getUserId2(),
-                    encounter.getUserId1(),
-                    encounter.getUserId2(),
-                    Timestamp.valueOf(encounter.getDate().atStartOfDay()),
-                    encounter.getHash());
-        });
+        encounters.getItems().get(0).forEach((encounter) -> jdbcTemplate.update("INSERT INTO encounter(hash, count, date, status, user_id_1, user_id_2) " +
+                        "VALUES (?, ?, ?, ?, ?, ?) " +
+                        "ON CONFLICT ON CONSTRAINT pk_encounter " +
+                        "DO UPDATE SET count = (SELECT count FROM encounter WHERE user_id_1 = ? AND user_id_2 = ?) + 1, date = ?, hash = ?",
+                encounter.getHash(),
+                encounter.getCount(),
+                Timestamp.valueOf(encounter.getDate().atStartOfDay()),
+                encounter.getStatus(),
+                encounter.getUserId1(),
+                encounter.getUserId2(),
+                encounter.getUserId1(),
+                encounter.getUserId2(),
+                Timestamp.valueOf(encounter.getDate().atStartOfDay()),
+                encounter.getHash()));
     }
 }
